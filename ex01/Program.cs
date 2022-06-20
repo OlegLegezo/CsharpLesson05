6c4af52992e9dc98ec2100cc70a0df6c7fe75acc
OlegLegezo <oleglegezo@yandex.ru>
2022-06-20 08:40:27 +0300

    цель лекции

---
+++ ex01/Program.cs
@@ -0,0 +1,7 @@
+﻿int a =12;
+Console.WriteLine(a.GetType());
+Console.WriteLine();
+var b =12;
+Console.WriteLine(b.GetType());
+ // много информации на лекции по визуализации кода и тд
+ // https://www.youtube.com/watch?v=psHY_6yV7DM&ab_channel=GeekBrains
